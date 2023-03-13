1ca131db453fb72a704ab7a13dd7fda7ce56f7ff
Your Name <fieldymus@gmail.com>
2023-03-13 22:37:58 +0300

    Expanded the alphabet main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
 pas = ''
 for x in range(11):
-    pas = pas + random.choice(list('abcdefghigklmnopqrstuvyxwz'))
+    pas = pas + random.choice(list('abcdefghigklmnopqrstuvyxwz1234567890'))
 print('Your password is: ', pas)
