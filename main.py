9aa74aa313a08d3f22fee30db76cb1cda03bffa1
Your Name <fieldymus@gmail.com>
2023-03-13 22:42:30 +0300

    Expanded the alphabet upper case main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 pas = ''
 for x in range(11):
-    pas = pas + random.choice(list('abcdefghigklmnopqrstuvyxwz'))
+    pas = pas + random.choice(list('abcdefghigklmnopqrstuvyxwz1234567890\
+                                   ABCDEFGHIGKLMNOPQRSTUVYXWZ'))
 print('Your password is: ', pas)
